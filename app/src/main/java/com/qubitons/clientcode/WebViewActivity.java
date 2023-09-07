fabe96f09060046c1e94e3c0e73e076f5a79a1a5
kiran <Kieanivare@gmail.com>
2023-09-07 22:29:02 +0530

    proper work in webview

--- app/src/main/java/com/qubitons/clientcode/WebViewActivity.java
+++ app/src/main/java/com/qubitons/clientcode/WebViewActivity.java
@@ -3,6 +3,7 @@ package com.qubitons.clientcode;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends Activity {
 
@@ -14,9 +15,12 @@ public class WebViewActivity extends Activity {
 
         String clientURL = getIntent().getStringExtra("client_url");
 
-        webView = (WebView) findViewById(R.id.webView1);
+        webView = findViewById(R.id.webView1);
         webView.getSettings().setJavaScriptEnabled(true);
+
+        // Set a WebViewClient to handle loading URLs within the WebView
+        webView.setWebViewClient(new WebViewClient());
+
         webView.loadUrl(clientURL);
     }
-
 }
